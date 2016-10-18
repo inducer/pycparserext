93623c148b8d87b434462d3732c1d9538c90da95
Andreas Kloeckner <inform@tiker.net>
2016-10-18 14:30:32 -0500

    PEP8 cleanliness

--- pycparserext/c_generator.py
+++ pycparserext/c_generator.py
@@ -2,6 +2,4 @@ from warnings import warn
 warn("pycparserext.c_generator is deprecated. Please use pycparser.c_generator "
         "directly.", DeprecationWarning)
 
-from pycparser.c_generator import *
-
-
+from pycparser.c_generator import *  # noqa
