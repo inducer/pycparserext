13fdb849861c1bf44eb9055f873a59b77e2febe6
Andreas Kloeckner <inform@tiker.net>
2024-07-01 16:24:44 -0500

    Drop deprecated c_generator

--- pycparserext/c_generator.py
+++
@@ -1,5 +0,0 @@
-from warnings import warn
-warn("pycparserext.c_generator is deprecated. Please use pycparser.c_generator "
-        "directly.", DeprecationWarning)
-
-from pycparser.c_generator import *  # noqa
